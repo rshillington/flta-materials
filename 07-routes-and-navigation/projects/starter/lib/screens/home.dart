99fa9e3115b38da50e02f931107f4ece443858af
Ralph Shillington <ralph.shillington@feenics.com>
2022-01-09 09:51:17 -0500

    chapter 7 complete

--- 07-routes-and-navigation/projects/starter/lib/screens/home.dart
+++ 07-routes-and-navigation/projects/starter/lib/screens/home.dart
@@ -90,7 +90,8 @@ class _HomeState extends State<Home> {
           ),
         ),
         onTap: () {
-          // TODO: home -> profile
+          Provider.of<ProfileManager>(context, listen: false)
+              .tapOnProfile(true);
         },
       ),
     );
